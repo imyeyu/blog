819c1ed4dbf788b0e1b903fd90c63d4a2bf3d630
夜雨 <imyeyu@qq.com>
2021-02-28 23:39:46 +0800

    日期格式化

---
+++ src/helpers/UnixTime.ts
@@ -0,0 +1,44 @@
+/**
+ * 补零
+ * 
+ * @param v 数值
+ * @param l 最终长度（默认 2）
+ */
+function paddingZero(v: number, l: number = 2) : string {
+	return (Array(l).join("0") + v).slice(-l);
+}
+
+/**
+ * Unix 时间戳转日期
+ * 
+ * @param unix 时间戳
+ */
+function toDate(unix: number) : string {
+	const d = new Date(unix);
+	return `${d.getFullYear()}-${paddingZero(d.getMonth() + 1)}-${paddingZero(d.getDate())}`;
+}
+
+/**
+ * Unix 时间戳转日常时间
+ * 
+ * @param unix 时间戳
+ */
+function toTime(unix: number) : string {
+	const d = new Date(unix);
+	return `${paddingZero(d.getHours())}:${paddingZero(d.getMinutes())}:${paddingZero(d.getSeconds())}`;
+}
+
+/**
+ * Unix 时间戳转日期和时间
+ * 
+ * @param unix 时间戳
+ */
+function toDateTime(unix: number) : string {
+	return `${toDate(unix)} ${toTime(unix)}`;
+}
+
+export default {
+	toDate,
+	toTime,
+	toDateTime
+}
