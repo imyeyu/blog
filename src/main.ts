4fa1f8e93d23cc5bafecfa0d27b81bd33b3ab84c
夜雨 <imyeyu@qq.com>
2021-02-28 23:19:09 +0800

    axios

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import axios from 'axios';
+
+axios.defaults.baseURL = `http://localhost:8081`;
+axios.interceptors.response.use((response) => response.data, (error) => {
+    throw error;
+});
 
 createApp(App).use(store).use(router).mount('#app');
