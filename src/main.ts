80e70b60f0cb512870d0a76be13ac3411532f632
夜雨 <imyeyu@qq.com>
2021-03-01 16:32:48 +0800

    跳转回滚顶部

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ import axios from 'axios';
 
 axios.defaults.baseURL = `http://localhost:8081`;
 axios.interceptors.response.use((response) => response.data, (error) => {
-    throw error;
+	throw error;
 });
+router.afterEach(() => window.scrollTo(0, 0));
 
 createApp(App).use(store).use(router).mount('#app');
