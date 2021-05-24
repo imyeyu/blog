1ddabe660664dedc7163fc95cb36bb8208682039
Nagiasu <imyeyu@qq.com>
2021-05-24 16:05:49 +0800

    注册 API 需要验证码

--- src/api/UserAPI.ts
+++ src/api/UserAPI.ts
@@ -5,8 +5,8 @@ async function signin(userSignin: UserSignin): Promise<Boolean> {
 	return axios.post('/user/signin', userSignin);
 }
 
-async function register(user: User): Promise<User> {
-	return axios.post('/user/register', user);
+async function register(user: User, captcha: string): Promise<User> {
+	return axios.post('/user/register', { data: user, captcha });
 }
 
 export default {
