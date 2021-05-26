afa9a6fc82ca0ea6e80c7ff2e584d00aa07b5f21
Nagiasu <imyeyu@qq.com>
2021-05-26 09:08:02 +0800

    时间差计算

--- src/helpers/UnixTime.ts
+++ src/helpers/UnixTime.ts
@@ -1,5 +1,41 @@
 import { paddingZero } from '@/helpers/Toolkit';
 
+function f(v : number) : number {
+	return Math.floor(v);
+}
+
+/**
+ * 计算时间差
+ * 
+ * @param begin 开始时间 
+ * @param end   结束时间（如果没有就是当前时间）
+ * @returns \{
+ *     l:  时间戳
+ *     y:  年
+ *     d:  日
+ *     h:  时
+ *     m:  分
+ *     s:  秒
+ *     ms: 毫秒
+ * \}
+ */
+ export function calcDifference(begin: Date, end?: Date) : any {
+	if (!end) {
+		end = new Date();
+	}
+
+	let cs = 1000, cm = 6E4, ch = 36E5, cd = 864E5, cy = 31536E6;
+	let l  = end.getTime() - begin.getTime();
+	let y  = f(l / cy),
+		d  = f((l / cd) - y * 365),
+		h  = f((l - (y * 365 + d) * cd) / ch),
+		m  = f((l - (y * 365 + d) * cd - h * ch) / cm),
+		s  = f((l - (y * 365 + d) * cd - h * ch - m * cm) / cs),
+		ms = f(((l - (y * 365 + d) * cd - h * ch - m * cm) / cs - s) * cs);
+
+	return { l, y, d, h, m, s, ms };
+}
+
 /**
  * Unix 时间戳转日期
  * 
